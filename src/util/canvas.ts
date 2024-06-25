bbd5dc6da86f7b4d3378697b3110b3f0a6427527
Jip Fr <jipfrijlink@gmail.com>
2024-06-25 14:16:46 +0200

    Add even/odd pattern on the board, formatting

--- src/util/canvas.ts
+++ src/util/canvas.ts
@@ -1,122 +1,131 @@
-import { Canvas, Image, loadImage } from 'canvas-constructor/skia';
-import { colors } from './colors';
+import { Canvas, Image, loadImage } from "canvas-constructor/skia";
+import { colors } from "./colors";
 
 const boardColors = {
-  background: '#36393E',
-  playerGradient: '#FFFFFF',
-  white: '#FFFFFF',
+	background: "#36393E",
+	pattern: "#36393E",
+	patternOdd: "#53565c",
+	playerGradient: "#FFFFFF",
+	white: "#FFFFFF",
 };
 
 export async function gameImageGenerator(context: {
-  players: {
-    userId: string;
-    avatarUrl: string;
-    x: number;
-    y: number;
-    lives: number;
-    range: number;
-    color: string;
-  }[];
-  width: number;
-  height: number;
+	players: {
+		userId: string;
+		avatarUrl: string;
+		x: number;
+		y: number;
+		lives: number;
+		range: number;
+		color: string;
+	}[];
+	width: number;
+	height: number;
 }) {
-  const playerImages: Record<string, Image> = {};
-  const canvas = new Canvas(context.width * 20, context.height * 20);
+	const playerImages: Record<string, Image> = {};
+	const canvas = new Canvas(context.width * 20, context.height * 20);
 
-  canvas
-    .setColor(boardColors.background)
-    .printRectangle(0, 0, canvas.width, canvas.height);
+	canvas
+		.setColor(boardColors.background)
+		.printRectangle(0, 0, canvas.width, canvas.height);
 
-  const cellWidth = Math.floor(canvas.width / context.width);
-  const cellHeight = Math.floor(canvas.width / context.width);
+	const cellWidth = Math.floor(canvas.width / context.width);
+	const cellHeight = Math.floor(canvas.width / context.width);
 
-  for await (const player of context.players) {
-    const image = await loadImage(player.avatarUrl);
-    playerImages[player.userId] = image;
-  }
+	for await (const player of context.players) {
+		const image = await loadImage(player.avatarUrl);
+		playerImages[player.userId] = image;
+	}
 
-  for (let y = 0; y < context.height; y++) {
-    for (let x = 0; x < context.width; x++) {
-      const closestPlayers = getClosestPlayers(context.players, x, y);
-      const closestPlayer = closestPlayers[0];
+	for (let y = 0; y < context.height; y++) {
+		for (let x = 0; x < context.width; x++) {
+			const closestPlayers = getClosestPlayers(context.players, x, y);
+			const closestPlayer = closestPlayers[0];
 
-      canvas.setColor(boardColors.white);
+			canvas.setColor(boardColors.white);
 
-      const alpha = Math.max((20 - closestPlayer.gradientRange) / 20, 0);
-      let relevantPlayers = closestPlayers.filter(
-        (v) => v.range <= v.player.range
-      );
+			// Grid pattern
+			let bgColor = boardColors.pattern;
+			if ((x % 2 === 0 && y % 2 === 1) || (x % 2 === 1 && y % 2 === 0))
+				bgColor = boardColors.patternOdd;
 
-      canvas.save();
-      canvas.translate(x * cellWidth, y * cellHeight);
-      if (closestPlayer.range !== 0) {
-        if (relevantPlayers.length > 0) {
-          const gradient = canvas.createLinearGradient(
-            0,
-            0,
-            cellWidth,
-            cellHeight
-          );
+			// Determine alpha
+			const alpha = Math.max((20 - closestPlayer.gradientRange) / 20, 0.2);
 
-          for (const [i, relevantPlayer] of relevantPlayers.entries()) {
-            let offset = 1 / relevantPlayers.length;
-            const color =
-              colors[relevantPlayer.player.color as keyof typeof colors];
+			let playersInRange = closestPlayers.filter(
+				(v) => v.range <= v.player.range
+			);
 
-            gradient.addColorStop(offset * i, color);
-            gradient.addColorStop(offset * (i + 1) - 0.05, color);
-          }
-          canvas
-            .setColor(gradient)
-            .printRectangle(0, 0, cellWidth - 1, cellHeight - 1);
-        } else {
-          canvas
-            .setGlobalAlpha(alpha)
-            .setColor(boardColors.playerGradient)
-            .printRectangle(0, 0, cellWidth - 1, cellHeight - 1);
-        }
-      }
-      canvas.restore();
+			canvas.save();
+			canvas.translate(x * cellWidth, y * cellHeight);
+			if (closestPlayer.range !== 0) {
+				if (playersInRange.length > 0) {
+					const gradient = canvas.createLinearGradient(
+						0,
+						0,
+						cellWidth,
+						cellHeight
+					);
 
-      if (closestPlayer.range === 0) {
-        canvas
-          .setGlobalAlpha(1)
-          .printImage(
-            playerImages[closestPlayer.player.userId],
-            x * cellWidth,
-            y * cellHeight,
-            cellWidth - 1,
-            cellHeight - 1
-          );
-      }
-    }
-  }
-  return canvas.toBufferAsync('png');
+					for (const [i, relevantPlayer] of playersInRange.entries()) {
+						let offset = 1 / playersInRange.length;
+						const color =
+							colors[relevantPlayer.player.color as keyof typeof colors];
+
+						gradient.addColorStop(offset * i, color);
+						gradient.addColorStop(offset * (i + 1) - 0.05, color);
+					}
+					canvas
+						.setColor(gradient)
+						.printRectangle(0, 0, cellWidth - 1, cellHeight - 1);
+				} else {
+					canvas
+						.setColor(bgColor)
+						.setGlobalAlpha(alpha)
+						.printRectangle(0, 0, cellWidth - 1, cellHeight - 1);
+				}
+			}
+			canvas.restore();
+
+			if (closestPlayer.range === 0) {
+				canvas
+					.setGlobalAlpha(1)
+					.printImage(
+						playerImages[closestPlayer.player.userId],
+						x * cellWidth,
+						y * cellHeight,
+						cellWidth - 1,
+						cellHeight - 1
+					);
+			}
+		}
+	}
+	return canvas.toBufferAsync("png");
 }
 
 function getClosestPlayers(
-  players: {
-    userId: string;
-    x: number;
-    y: number;
-    avatarUrl: string;
-    lives: number;
-    range: number;
-    color: string;
-  }[],
-  x: number,
-  y: number
+	players: {
+		userId: string;
+		x: number;
+		y: number;
+		avatarUrl: string;
+		lives: number;
+		range: number;
+		color: string;
+	}[],
+	x: number,
+	y: number
 ) {
-  return players
-    .filter((player) => player.lives > 0)
-    .map((player) => {
-      const distanceX = Math.abs(x - player.x);
-      const distanceY = Math.abs(y - player.y);
-      return {
-        player,
-        gradientRange: Math.sqrt(distanceX * distanceX + distanceY * distanceY),
-        range: Math.max(distanceX, distanceY),
-      };
-    })
-    .sort((a, b) => a.gradientRange - b.gradientRange);
+	return players
+		.filter((player) => player.lives > 0)
+		.map((player) => {
+			const distanceX = Math.abs(x - player.x);
+			const distanceY = Math.abs(y - player.y);
+			return {
+				player,
+				gradientRange: Math.sqrt(distanceX * distanceX + distanceY * distanceY),
+				range: Math.max(distanceX, distanceY),
+			};
+		})
+		.sort((a, b) => a.gradientRange - b.gradientRange);
 }
